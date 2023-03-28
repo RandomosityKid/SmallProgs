0314b57d50c552c17bbbde432544d4b29faf72ca
RandomosityKid <brant_dc@yahoo.com>
2023-03-28 15:32:54 -0700

    Added quickstort

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,3 +1,20 @@
 public class QuickSort {
-	
+	public static void main(String arge[]) {
+		String a = "Car";
+		String b = "Apple";
+		String c = "Banana";
+		
+		System.out.println(getMedian(a,b,c));
+		
+	}
+
+	private static String getMedian(String a, String b, String c) {
+		int x = a.compareToIgnoreCase(b);
+		int y = b.compareToIgnoreCase(c);
+		int z = a.compareToIgnoreCase(c);
+		
+		if(x*y > 0) return b;
+		if(x*z > 0) return c;
+		return a;
+	}
 }
