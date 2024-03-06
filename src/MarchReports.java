e5cd384d6ecd6108ee24b38fe44141c634db7302
RandomosityKid <brant_dc@yahoo.com>
2024-03-05 16:08:15 -0800

    Created March Reports Program
    Annual downloading of Reports, Chrome Screen half on right, after downloading last pdf and changing folder directory, program will download the remaining 1-5, then close tabs for next

---
+++ src/MarchReports.java
@@ -0,0 +1,65 @@
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+
+public class MarchReports {
+	static Robot r;
+	static Thread t;
+	
+	public static void main(String arge[]) throws AWTException, InterruptedException {
+		r = new Robot();
+		r.mouseMove(1800, 170);
+		mClick();
+		for (int i = 0; i < 5; i++) {
+			closeWindow();
+			mClick();
+			enter();
+			if (i == 4) {
+				wait(50);
+				break;
+			}
+			wait(200);
+		}
+		closeWindow();
+		wait(250);
+		closeWindow();
+		wait(250);
+		closeWindow();
+	}
+	
+	private static void initRobot() throws AWTException{
+		if(r == null){
+			r = new Robot();
+		}
+	}
+	
+	private static void wait(int speed) throws InterruptedException{
+		t.sleep(speed);
+	}
+	
+	private static void mClick() throws InterruptedException, AWTException {
+		initRobot();
+		wait(300);
+		r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
+		wait(100);
+		r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+	}
+	
+	private static void closeWindow() throws AWTException, InterruptedException{
+		initRobot();
+		r.keyPress(KeyEvent.VK_CONTROL);
+		r.keyPress(KeyEvent.VK_W);
+		r.keyRelease(KeyEvent.VK_W);
+		r.keyRelease(KeyEvent.VK_CONTROL);
+	}
+	
+	private static void enter() throws AWTException, InterruptedException{
+		initRobot();
+		wait(750);
+		r.keyPress(KeyEvent.VK_ENTER);
+		r.keyRelease(KeyEvent.VK_ENTER);
+	}
+}
+
